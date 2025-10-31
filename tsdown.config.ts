fde9ada499ae677caa85e1446c929c6f1c1ddaf5
onmax <maximogarciamtnez@gmail.com>
2025-10-31 10:44:53 +0100

    feat: add build configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: [
     'src/index.ts',
+    'src/adapters/tutorialkit.ts',
   ],
   dts: true,
+  clean: true,
+  sourcemap: true,
 })
